c3581cc7872e1b400c1d55e738486c76c3185d72
yshwang <yshwang@sphereax.com>
2024-11-29 17:50:44 +0900

    00.스프링 초기 세팅

---
+++ src/test/java/org/in28minutes/learnspringframework/SutdySpringUApplicationTests.java
@@ -0,0 +1,13 @@
+package org.in28minutes.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutdySpringUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

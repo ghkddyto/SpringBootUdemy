b90a9fd7e5a6b685d2517f7835e4440bf4217b5f
yshwang <dyto153@naver.com>
2024-12-06 11:04:17 +0900

    패키지 정리

--- src/main/java/com/in28minutes/learnspringframework02/game/GaimingAppLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework02/game/GaimingAppLauncherApplication.java
@@ -1,18 +1,16 @@
-package com.in28minutes.learnspringframework02;
+package com.in28minutes.learnspringframework02.game;
 
-import com.in28minutes.learnspringframework02.game.GamingConsole;
-import com.in28minutes.learnspringframework02.game.GameRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.in28minutes.learnspringframework02.game")
-public class GamingAppLauncherApplication {
+public class GaimingAppLauncherApplication {
     public static void main(String[] args){
         try (var context =
                      new AnnotationConfigApplicationContext
-                             (GamingAppLauncherApplication.class)){
+                             (GaimingAppLauncherApplication.class)){
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
